9415641794e7cfdb14a32cacf8513f41457dc394
simple <simple@123.com>
2020-06-29 16:42:55 +0800

    【测试】使用idea向本地仓库提交java工程

---
+++ git-first/src/com/simple/git/Demo1.java
@@ -0,0 +1,17 @@
+package com.simple.git;
+
+/**
+ * Created by hgy on 2020/6/29.
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Idea Git project ");
+        System.out.println("Hello Idea Git project ");
+        System.out.println("Hello Idea Git project ");
+        System.out.println("Hello Idea Git project ");
+        System.out.println("Hello Idea Git project ");
+    }
+
+
+}
